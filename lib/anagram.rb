5a523f44469c2804691bc94694094a6a7bc75b00
Mars Le <hp.mars.le@gmail.com>
2019-09-01 18:25:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(array)
+    
+    
+    #return all matches in array or an empty array if no matches
+  end 
+  
+  
+end 
